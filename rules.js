18cee799685dd6ba8a412780c5f2736fba34a571
Alex Stasiewicz <adlerzz.true@gmail.com>
2025-11-03 00:51:36 +0300

    add more operators fix order

--- rules.js
+++ rules.js
@@ -1,28 +1,39 @@
 const q = (...types) => Object.fromEntries(types.map(type => [type, type]))
+const Q = (...types) => Object.fromEntries(types.map(type => [type, type.toLowerCase()]))
+
 
 const rules = {
     "LINE_COMMENT": /\/\/.*/,
     "BLOCK_COMMENT": /\/\*.*\*\//,
     "STRING": /\"([^\\]|\\["\r\n])*?\"/,
-    "VAR": "var",
-    "THE": "the",
+    
+    ...Q("VAR", "THE", "FOR", "IF", "WHILE", "MATCH", 
+        "BREAK", "CONTINUE", "NULL", "IMPORT", "EXPORT",
+        "RETURN"),
+    
+    "ID": /[_$@#A-Za-z][_$@#A-Za-z0-9]*/,
+    
+    ...q("+=", "-=", "*=", "/="),
+    ...q("!=", "==", "<=", ">="),
+    ...q("=>>", "=>", "<<", ">>", "|>>", "|>", "|?"),
+    ...q("..", "?.", "??"),
+    ...q("+", "-", "*", "/", "!", "&", "|", "~"),
+    ...q(";", ":", ".", ",", "[", "]", "(", ")", "{", "}"),
+    
     "HEX": [
         /-?0[xX][A-Fa-f0-9]+/,
         (s) => parseInt(s)
     ],
     "NUM": [
-        /-?\d+(\.\d*)?/,
+        /-?\d+(\.\d+)?/,
         (s) => parseFloat(s)
     ],
-    "ID": /[_$@#A-Za-z][_$@#A-Za-z0-9]*/,
-    
-    ...q("=", ";", ",", "[", "]", "(", ")", "{", "}"),
-    
+
     "BLANK": [
         /\s+/, 
         () => null
     ],
-    
+
     "?": /.|\r|\n/,
 };
 
